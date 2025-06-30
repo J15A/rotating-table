2a8cdb8a646e340f4defa0895bca043d5b80e5eb
Jack A <jack.ambrose489@gmail.com>
2025-06-29 21:03:29 -0500

    Updated Win message UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,23 +8,19 @@ using UnityEngine.Rendering;
 
 public class GameManager : MonoBehaviour
 {
-    public int padNumber = 4;
+    public int padNumber = 3;
     public int handNumber = 2;
-    
-    // Initialize to -1 to prevent out of bounds error on ResetGame call without filling the necessary fields
-    private int nextPadNumber = -1;
-    private int nextHandNumber = -1;
 
-    public int choosenDepth = 1; // Depth for the minimax algorithm
-
-    public Image[] padColours;
-    public Button[] padButtons; // Assuming you have buttons for each pad
+    private Image[] padColours;
+    private Button[] padButtons; // Assuming you have buttons for each pad
 
     public int[] pads;
-    public bool isPlayerTurn1;
-    public bool isPlayerTurn2;
+    private bool isPlayerTurn1;
+    private bool isPlayerTurn2;
 
-    public TMP_Text winMessage;
+    public GameObject endGameMessage;
+    public TextMeshProUGUI endGameHeader;
+    public TextMeshProUGUI endGameBody;
 
     public int[] padPresses; // Counter for the number of pad presses
     public int distinctPadPresses;
@@ -47,7 +43,7 @@ public class GameManager : MonoBehaviour
         distinctPadPresses = 0;
 
         // Hide the win message initially
-        winMessage.gameObject.SetActive(false);
+        endGameMessage.SetActive(false);
 
         // Initialize the pads array with the specified number of pads
         padColours = new Image[padNumber];
@@ -67,25 +63,22 @@ public class GameManager : MonoBehaviour
 
         for (int i = 0; i < padNumber; i++)
         {
-            // Assuming you have assigned the pad colours in the inspector
             if (padNumber == 3)
             {
-                padColours[i] = GameObject.Find("Pad" + (i + 1)).GetComponent<Image>();
-                padButtons[i] = GameObject.Find("Pad" + (i + 1)).GetComponent<Button>();
+                padColours[i] = games[0].transform.GetChild(i+1).GetComponent<Image>();
+                padButtons[i] = games[0].transform.GetChild(i+1).GetComponent<Button>();
             }
             else if (padNumber == 4)
             {
-                padColours[i] = GameObject.Find("Pad" + (i + 4)).GetComponent<Image>();
-                padButtons[i] = GameObject.Find("Pad" + (i + 4)).GetComponent<Button>();
+                padColours[i] = games[1].transform.GetChild(i+1).GetComponent<Image>();
+                padButtons[i] = games[1].transform.GetChild(i+1).GetComponent<Button>();
             }
             else if (padNumber == 5)
             {
-                padColours[i] = GameObject.Find("Pad" + (i + 8)).GetComponent<Image>();
-                padButtons[i] = GameObject.Find("Pad" + (i + 8)).GetComponent<Button>();
+                padColours[i] = games[2].transform.GetChild(i+1).GetComponent<Image>();
+                padButtons[i] = games[2].transform.GetChild(i+1).GetComponent<Button>();
             }
         }
-
-
 
         pads = new int[padNumber];
         // Initialize the pads with random colors
@@ -114,107 +107,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    /* Minimax algorithm that was unecessary
-    public double minimax(gameState position, int depth, bool isMaximizingAI)
-    {
-        // Implement the minimax algorithm here
-        Debug.Log("executing minimax");
-
-        // Base case: if depth is 0 or game over condition is met
-        bool gameOver = true;
-        for (int i = 0; i < position.pads.Length; i++)
-        {
-            if (position.pads[i] != position.pads[0]) // Check if all pads are the same
-            {
-                gameOver = false;
-                break;
-            }
-        }
-        if (depth == 0 || gameOver)
-        {
-            return evaluatePosition(position); // Return the evaluation score
-        }
-        // Recursive case: explore child nodes
-        if (isMaximizingAI)
-        {
-            double maxEval = double.NegativeInfinity;
-            // Generate all possible moves for the maximizing player
-            for (int newPadStart = 0; newPadStart < position.pads.Length; newPadStart++)
-            {
-                gameState childState = new gameState
-                {
-                    pads = new int[padNumber], // Create a new pads array
-                    padsPresses = new int[padNumber] // Create a new padsPressed array
-                };
-                int count = 0;
-                for (int i = newPadStart; i < newPadStart + padNumber; i++)
-                {
-                    if (count >= padNumber) break; // Ensure we don't exceed the array size
-                    childState.pads[count] = position.pads[i % padNumber];
-                    childState.padsPresses[count] = 0;
-                    count++;
-                }
-                double eval = minimax(childState, depth - 1, false); // Recursive call for the child node
-                maxEval = Math.Max(maxEval, eval); // Update the maximum evaluation score
-            }
-            return maxEval; // Return the maximum evaluation score for the maximizing player
-        }
-        else
-        {
-            double minEval = double.PositiveInfinity;
-
-            bool padsPressesEmpty = true;
-            for (int i = 0; i < position.padsPresses.Length; i++)
-            {
-                if (position.padsPresses[i] > 0)
-                {
-                    padsPressesEmpty = false;
-                    break;
-                }
-            }
-            if (padsPressesEmpty!)
-            {
-                // Find which pads can be changed colour
-                int[] padsPressed = new int[handNumber];
-                int count = 0;
-                for (int i = 0; i < position.padsPresses.Length; i++)
-                {
-                    if (position.padsPresses[i] > 0)
-                    {
-                        padsPressed[count] = i;
-                        count++;
-                    }
-                }
-
-                // Generate all permutations of colours for the pressed pads
-                int total = 1 << handNumber; // 2^handNumber
-                // Iterate through all combinations of pressed pads
-                for (int i = 0; i < total; i++)
-                {
-                    gameState childState = new gameState
-                    {
-                        pads = (int[])position.pads.Clone(), // Clone the current pads state
-                        padsPresses = new int[padNumber] // Reset the padsPresses array
-                    };
-
-                    //Find all possible colourings of the pressed pads
-                    int[] bits = new int[handNumber];
-                    for (int j = 0; j < handNumber; j++)
-                    {
-                        bits[handNumber - j - 1] = (i >> j) & 1;
-                    }
-                    for (int j = 0; j < handNumber; j++)
-                    {
-                        int padIndex = padsPressed[j];
-                        childState.pads[padIndex] = bits[j]; // Change the colour of the pressed pad
-                        double eval = minimax(childState, depth - 1, true); // Recursive call for the child node
-                        minEval = Math.Min(minEval, eval); // Update the minimum evaluation score
-                    }
-                }
-            }
-            return minEval; // Return the minimum evaluation score for the minimizing player
-        }
-    }*/
     public int[] GetBestMovePads(gameState position)
     {
         double maxEval = double.NegativeInfinity;
@@ -424,7 +316,9 @@ public class GameManager : MonoBehaviour
                     padColours[i].color = winningColor;
                 }
                 //show the win message
-                winMessage.gameObject.SetActive(true);
+                endGameMessage.SetActive(true);
+                endGameHeader.text = "You Win!";
+                endGameBody.text = "You have found the strategy that guarantees a win. Well done!";
                 return;
             }
             // If the player has pressed enough pads, they can end their turn
@@ -450,24 +344,19 @@ public class GameManager : MonoBehaviour
 
     public void ResetGame()
     {
-        if (nextHandNumber == -1) // prevent index out of bounds exception
+        if (handNumber > padNumber)
         {
-            Start();
+            handNumber = padNumber; // Ensure hand number does not exceed pad number
         }
-        else
-        {
-            handNumber = nextHandNumber;
-            padNumber = nextPadNumber;
-            Start();
-        }
+        Start();
     }
 
     public void changeHandNumber(int newHandNumber)
     {
-        nextHandNumber = newHandNumber;
+        handNumber = newHandNumber + 1;
     }
     public void changePadNumber(int newPadNumber)
     {
-        nextPadNumber = newPadNumber;
+        padNumber = newPadNumber + 3;
     }
 }   
